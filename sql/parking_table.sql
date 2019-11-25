c2cfbc8579554aa3898497f6797a4c54f6a1097c
dganin <deborahganin@gmail.com>
2019-11-25 16:44:27 +0100

    Correction de scripts de la BDD

--- sql/parking_table.sql
+++ sql/parking_table.sql
@@ -1,51 +1,76 @@
 DROP DATABASE IF EXISTS PARKING;
 CREATE DATABASE PARKING;
 USE PARKING;
-DROP TABLE IF EXISTS MEMBRE;
+DROP TABLE IF EXISTS LIGUE;
+DROP TABLE IF EXISTS UTILISATEUR;
 DROP TABLE IF EXISTS RESERVATION;
 DROP TABLE IF EXISTS PLACE;
 DROP TABLE IF EXISTS ATTENTE;
 
 
-CREATE TABLE MEMBRE
+
+/*CREATE TABLE PERSONNE(
+   id_membre VARCHAR(50),
+   pseudo VARCHAR(50),
+   mdp VARCHAR(50),
+   PRIMARY KEY(id_membre)
+);*/
+
+/*CREATE TABLE ADMIN(
+   id_membre VARCHAR(50),
+   PRIMARY KEY(id_membre),
+   FOREIGN KEY(id_membre) REFERENCES PERSONNE(id_membre)
+);*/ 
+
+CREATE TABLE LIGUE
 (
-   id_membre VARCHAR(2) NOT NULL,
-   pseudo_membre VARCHAR(30),
-   mdp_membre VARCHAR(100),
-   nom_membre VARCHAR(30),
-   prenom_membre VARCHAR(30),
-   poste_membre VARCHAR(30),
-   email_membre VARCHAR(50),
-   plaque_vehicule VARCHAR(8),
-   voiture_pmr BOOLEAN,
-   constraint pk_MEMBRE PRIMARY KEY(id_membre)
+   id_ligue int(2),
+   nom_ligue varchar(50),
+   constraint pk_LIGUE PRIMARY KEY(id_ligue)
 )
-ENGINE=INNODB; 
+ENGINE=INNODB;
+
+CREATE TABLE UTILISATEUR
+(
+   id_membre int(2) not null,
+   nom_membre VARCHAR(50) not null,
+   prenom_membre VARCHAR(50) not null,
+   poste_membre VARCHAR(50),
+   email_membre VARCHAR(50) not null,
+   mdp_membre VARCHAR(100) not null,
+   plaque_vehicule VARCHAR(8) not null,
+   voiture_pmr boolean not null,
+   access_level boolean not null,
+   constraint pk_UTILISATEUR PRIMARY KEY(id_membre)
+)
+ENGINE=INNODB;
 
 CREATE TABLE RESERVATION
 (
-   id_reserv VARCHAR(2) NOT NULL,
-   date_debut_reserv DATE,
-   date_fin_reserv DATE,
+   id_reserv int(2) not null,
+   date_debut_reserv date,
+   date_fin_reserv date,
+   id_membre int(2) NOT NULL,
    constraint pk_RESERVATION PRIMARY KEY(id_reserv)
 )
-ENGINE=INNODB; 
+ENGINE=INNODB;
 
 CREATE TABLE PLACE
 (
-   id_place VARCHAR(3) NOT NULL,
-   num_place INT(2),
+   id_place int(2) not null,
+   num_place int(2),
    type_place VARCHAR(30),
-   etat_place BOOLEAN,
+   etat_place boolean,
+   id_reserv int(2) NOT NULL,
    constraint pk_PLACE PRIMARY KEY(id_place)
 )
-ENGINE=INNODB; 
+ENGINE=INNODB;
 
 CREATE TABLE ATTENTE
 (
-   id_attente VARCHAR(3) NOT NULL,
-   date_debut_attente VARCHAR(50),
-   priorite_attente VARCHAR(50),
-   constraint pk_ATTENTE PRIMARY KEY(id_attente)
+   rang int(2) not null,
+   priorite_attente boolean,
+   id_membre int(2) NOT NULL,
+   constraint pk_ATTENTE PRIMARY KEY(rang)
 )
 ENGINE=INNODB;
