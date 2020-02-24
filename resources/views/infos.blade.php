fd4b59ebc29315374d6db096e0116c1bbf874952
dganin <deborahganin@gmail.com>
2020-02-24 14:00:39 +0100

    resolved security issu with dependencie

--- resources/views/infos.blade.php
+++ resources/views/infos.blade.php
@@ -1,3 +1,19 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulaire </title>
+</head>
+<body>
+<form method="POST" action="http://monsite.fr/users" accept-charset="UTF-8">
+    <input name="_token" type="hidden" value="pV1vWWdUqFDfYsBjKag43C3NvzbIC0lHtMnv9BpI">    
+    <label for="nom">Entrez votre nom : </label>    
+    <input name="nom" type="text" id="nom">    
+    <input type="submit" value="Envoyer !"> 
+</form>
+</body>
+</html>  
 @extends('template')
 
 @section('titre')
@@ -11,10 +27,3 @@
         {!! Form::submit('Envoyer !') !!}
     {!! Form::close() !!}
 @endsection
-
-<form method="POST" action="http://monsite.fr/users" accept-charset="UTF-8">
-    <input name="_token" type="hidden" value="pV1vWWdUqFDfYsBjKag43C3NvzbIC0lHtMnv9BpI">    
-    <label for="nom">Entrez votre nom : </label>    
-    <input name="nom" type="text" id="nom">    
-    <input type="submit" value="Envoyer !"> 
-</form>
